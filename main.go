b16f5e16fe53a16465e6da6cd6a365aca8ed1c3e
Tyler Kuhn <tyler@wargoats.com>
2018-04-19 15:15:47 -0700

    added first cobra build

--- main.go
+++ main.go
@@ -2,53 +2,102 @@ package main
 
 import (
   "fmt"
-  "strings"
+  //"strings"
 
   "github.com/spf13/cobra"
 )
 
 func main() {
-  var echoTimes int
-
-  var cmdPrint = &cobra.Command{
-    Use:   "print [string to print]",
-    Short: "Print anything to the screen",
-    Long: `print is for printing anything back to the screen.
-For many years people have printed back to the screen.`,
-    Args: cobra.MinimumNArgs(1),
+  var cmdClean = &cobra.Command{
+    Use:   "clean",
+    Short: "deletes the blockchain data",
+    //Long: `print is for printing anything back to the screen.`
     Run: func(cmd *cobra.Command, args []string) {
-      fmt.Println("Print: " + strings.Join(args, " "))
+      //rm "$NODEOS_PATH/config/genesis.json"
+      //rm -rf "$NODEOS_PATH/data"
+      fmt.Println("cleaned")
     },
   }
 
-  var cmdEcho = &cobra.Command{
-    Use:   "echo [string to echo]",
-    Short: "Echo anything to the screen",
-    Long: `echo is for echoing anything back.
-Echo works a lot like print, except it has a child command.`,
-    Args: cobra.MinimumNArgs(1),
+  var cmdReset = &cobra.Command{
+    Use:   "reset",
+    Short: "deletes the blockchain data & starts nodeos",
     Run: func(cmd *cobra.Command, args []string) {
-      fmt.Println("Print: " + strings.Join(args, " "))
+      //cmdClean.Execute()
+      //nodeos
+      fmt.Println("reset")
     },
   }
 
-  var cmdTimes = &cobra.Command{
-    Use:   "times [# times] [string to echo]",
-    Short: "Echo anything to the screen more times",
-    Long: `echo things multiple times back to the user by providing
-a count and a string.`,
-    Args: cobra.MinimumNArgs(1),
+  var cmdWallet = &cobra.Command{
+    Use:   "wallet",
+    Short: "creates the default wallet",
+    Args: cobra.MinimumNArgs(0),
     Run: func(cmd *cobra.Command, args []string) {
-      for i := 0; i < echoTimes; i++ {
-        fmt.Println("Echo: " + strings.Join(args, " "))
+      if (len(args) == 0) {
+        //eos_wallet_save "default" $(cleos wallet create | grep PW)
+        fmt.Println("wallet default created")
+      } else {
+        //eos_wallet_save $1, $(cleos wallet create -n $1 | grep PW)
+        fmt.Println("wallet", args[0], "created")
       }
     },
   }
 
-  cmdTimes.Flags().IntVarP(&echoTimes, "times", "t", 1, "times to echo the input")
+  var cmdSetup = &cobra.Command{
+    Use:   "setup",
+    Short: "loads the BIOS",
+    Run: func(cmd *cobra.Command, args []string) {
+      //cmdWallet.Execute()
+      //cleos set contract eosio "$EOS_PATH/build/contracts/eosio.bios" -p eosio
+      fmt.Println("setup")
+    },
+  }
 
-  var rootCmd = &cobra.Command{Use: "app"}
-  rootCmd.AddCommand(cmdPrint, cmdEcho)
-  cmdEcho.AddCommand(cmdTimes)
+  var cmdAccount = &cobra.Command{
+    Use:   "account",
+    Short: "creates a contract account",
+    Args: cobra.MinimumNArgs(1),
+    Run: func(cmd *cobra.Command, args []string) {
+      /*
+      if ! (cleos wallet keys | grep EOS); then
+        //cmdSetup.Execute()
+      fi
+
+      KEY=$(cleos wallet keys | grep EOS | cut -d '"' -f 2)
+      cleos create account eosio $1 $KEY $KEY
+      */
+      fmt.Println("account", args[0], "created")
+    },
+  }
+
+  var cmdBuild = &cobra.Command{
+    Use:   "build",
+    Short: "builds a contract (wast & abi)",
+    Args: cobra.MinimumNArgs(1),
+    Run: func(cmd *cobra.Command, args []string) {
+      //eosiocpp -o $1.wast $1.cpp
+      //eosiocpp -g $1.abi $1.cpp
+      fmt.Println("built", args[0])
+    },
+  }
+
+  var cmdDeploy = &cobra.Command{
+    Use:   "deploy",
+    Short: "builds & deploys a contract",
+    Args: cobra.MinimumNArgs(1),
+    Run: func(cmd *cobra.Command, args []string) {
+      /*
+      cd $1
+      //cmdBuild.Execute()
+      cd ..
+      cleos set contract $1 $1
+      */
+      fmt.Println("deployed", args[0])
+    },
+  }
+
+  var rootCmd = &cobra.Command{Use: "zeos"}
+  rootCmd.AddCommand(cmdClean, cmdReset, cmdWallet, cmdSetup, cmdAccount, cmdBuild, cmdDeploy)
   rootCmd.Execute()
 }
